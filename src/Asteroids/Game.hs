a6d12806fd8df16fa67363715fb882ba6fbc8446
James King <james@agentultra.com>
2022-11-08 22:36:04 -0500

    Add ship rotation to the player ship
    We can now rotate points around an origin point and use that to rotate
    the points around the player to render the ship.

--- src/Asteroids/Game.hs
+++ src/Asteroids/Game.hs
@@ -4,6 +4,8 @@
 module Asteroids.Game where
 
 import Control.Monad (unless)
+import Data.Vector.Storable (Vector)
+import qualified Data.Vector.Storable as V
 import Foreign.C.Types
 import Linear
 import qualified SDL
@@ -22,11 +24,13 @@ delta = 0.0
 
 data GameState
   = GameState
-  { gameStateRenderer    :: SDL.Renderer
-  , gameStateTicks       :: Double
-  , gameStateCurrentTime :: Double
-  , gameStateAccumulator :: Double
-  , gameStatePlayerShip  :: V2 CInt
+  { gameStateRenderer       :: SDL.Renderer
+  , gameStateTicks          :: Double
+  , gameStateCurrentTime    :: Double
+  , gameStateAccumulator    :: Double
+  , gameStatePlayerPosition :: V2 CInt
+  , gameStatePlayerSize     :: CInt
+  , gameStatePlayerRotation :: Float
   }
   deriving (Eq, Show)
 
@@ -38,6 +42,8 @@ initGameState renderer
   <*> SDL.time
   <*> pure 0.0
   <*> pure (V2 20 20)
+  <*> pure 10
+  <*> pure 0.0
 
 run :: IO ()
 run = do
@@ -75,7 +81,7 @@ update :: GameState -> GameState
 update state@GameState {..} =
   let nextState = state { gameStateAccumulator = gameStateAccumulator - delta
                         , gameStateTicks = gameStateTicks + delta
-                        , gameStatePlayerShip = gameStatePlayerShip + V2 1 0
+                        , gameStatePlayerRotation = gameStatePlayerRotation + 0.01
                         }
   in
     if gameStateAccumulator >= delta
@@ -87,7 +93,32 @@ render GameState {..} = do
   SDL.rendererDrawColor gameStateRenderer $= V4 0 0 0 255
   SDL.clear gameStateRenderer
 
-  let shipRect = SDL.Rectangle (SDL.P gameStatePlayerShip) (V2 20 20)
-  SDL.rendererDrawColor gameStateRenderer $= V4 255 0 0 255
-  SDL.fillRect gameStateRenderer (Just shipRect)
+  renderPlayerShip
+    gameStatePlayerPosition
+    gameStatePlayerSize
+    gameStatePlayerRotation
+    gameStateRenderer
   SDL.present gameStateRenderer
+
+renderPlayerShip :: V2 CInt -> CInt -> Float -> SDL.Renderer -> IO ()
+renderPlayerShip position size rotation renderer = do
+  SDL.rendererDrawColor renderer $= V4 0 255 0 255
+  let shipPoints = V.map (rotateOrigin position rotation) . playerShipPoints position $ size
+  SDL.drawLines renderer shipPoints
+
+playerShipPoints :: V2 CInt -> CInt -> Vector (SDL.Point V2 CInt)
+playerShipPoints position size
+  = V.fromList [ SDL.P $ position + V2 0 (-size)
+               , SDL.P $ position + V2 (size `div` 2) size
+               , SDL.P $ position + V2 (-(size `div` 2)) size
+               , SDL.P $ position + V2 0 (-size)
+               ]
+
+rotateOrigin :: V2 CInt -> Float -> SDL.Point V2 CInt -> SDL.Point V2 CInt
+rotateOrigin origin@(V2 originX originY) rotation (SDL.P inputP) =
+  let s = sin rotation
+      c = cos rotation
+      (SDL.P (V2 pointX pointY)) = SDL.P $ inputP - origin
+      x = fromIntegral pointX * c - fromIntegral pointY * s
+      y = fromIntegral pointX * s + fromIntegral pointY * c
+  in SDL.P (V2 (round x + originX) (round y + originY))
